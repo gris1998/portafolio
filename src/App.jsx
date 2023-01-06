9da8ff49dd69888310ef7213eff4a7723233b6c3
grisel1998 <griselpulvirenti@gmail.com>
2023-01-06 12:20:34 -0500

    portafolio

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './components/Header'
+import "./App.css"
+function App() {
+
+
+  return (
+    <div className="App">
+      <Header/>
+      <h1>hola mundo</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import logo from"../assets/react.svg"
+const Header = () => {
+  return (
+    <div><img src={logo} alt="" /></div>
+  )
+}
+
+export default Header
